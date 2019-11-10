ae94441ba902951bee31cd30e4384be426e282cc
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2019-11-10 15:42:06 +0100

    Update

--- scrape.py
+++ scrape.py
@@ -1,12 +1,22 @@
 from bs4 import BeautifulSoup
 import sys
+import os
 
 soup = BeautifulSoup(open(sys.argv[1]), "html5lib")
 lis = soup.html.body.find_all("li",recursive=False)
 
+i=0
 for li in lis:
-    aa = li.find_all('a')
-    if len(aa)==1:
-        print(li.a['href'].replace('./',sys.argv[2]))
+    if i % 100==0:
+        aa = li.find_all('a')
+        if len(aa)==1:
+            path = li.a['href']
+            filename, ext = os.path.splitext(path)
+            folder_name = filename.replace('./','')
+            code_path = path.replace('./',sys.argv[2])
+            desc = li.text 
+            print(code_path,ext,folder_name)
+            print(desc)
+    i=i+1
 
 
